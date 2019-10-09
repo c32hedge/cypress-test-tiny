b737fa55ed1053c174fa60ddce87ff9e375062d0
c32hedge <ccrandal@grantham.edu>
2019-10-09 12:18:00 -0400

    Test code to reproduce #5319

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,15 @@
-describe('page', () => {
-  it('works', () => {})
-})
+describe('sample', () => {
+  before(function() {
+    console.log('Before code called!');
+  });
+
+  it("takes some time and logs to the console", () => {
+    console.log("running tests");
+    cy.wait(5000);
+  });
+
+  it("shouldn't re-run the before", () => {
+    cy.visit('https://www.google.com');
+    console.log("if the 'running tests' line is not present in the console, but 'Before code called!' is, we ran the before block again.");
+  });
+});
